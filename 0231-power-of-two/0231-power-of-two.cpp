f2367e40c9009a299e3a248a8c327c2a9d5b93e9
Kirubel567 <kirubel.wubet1996@gmail.com>
2025-03-31 23:31:33 +0300

    Time: 0 ms (100%), Space: 7.8 MB (83.83%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) { 
+        for (int m = 0; pow(2, m) <= n; m++){
+            if (n == pow(2, m)){
+            return true; 
+            break; 
+        }
+        }
+        return false; 
+        
+    }
+};
