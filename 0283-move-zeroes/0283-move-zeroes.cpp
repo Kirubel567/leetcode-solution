459bba1b7b11607f4849b19829c0ede49d617ed8
Kirubel567 <kirubel.wubet1996@gmail.com>
2025-04-10 13:22:31 +0300

    Time: 0 ms (100%), Space: 24 MB (17.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-         int nonZeroIndex = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[nonZeroIndex]);
-                nonZeroIndex++;
-            }
+        int left = 0; 
+        int right = 0;
+
+        while(right < nums.size()){
+            if (nums[right] != 0){
+                swap(nums[right], nums[left]); 
+                left++; 
+                right++;
+            }else{right++;} 
         }
 
-        
-    }
+}
+    
 };
+// iteration one: right 1 and left 0
+// iteration two: 
