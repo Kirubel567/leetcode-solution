92ba44f21286d389b1c2984758ef82ec38c34907
Kirubel567 <kirubel.wubet1996@gmail.com>
2025-04-12 22:07:09 +0300

    Time: 0 ms (100%), Space: 18.7 MB (95.5%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.cpp
+++ 1651-shuffle-string/1651-shuffle-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
+        
         string t = s; 
         for (int i = 0; i < s.size(); i++){
             t[indices[i]] = s[i];   
