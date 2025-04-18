8c7288d296a5eea07e269a4497669de19b2de9d6
Kirubel567 <kirubel.wubet1996@gmail.com>
2025-04-18 21:52:42 +0300

    Time: 0 ms (100%), Space: 8 MB (15.64%) - LeetHub

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,34 +1,7 @@
 class Solution {
 public:
     int minimumSum(int num) {
-        // int Sum = 0; 
-        // int num1, num2;
-        // string numString1, numString2; 
-        // int n1, n2;
-
-
-
-        // string word = to_string(num); 
-        // for (int i = 0; i < 2; i++){
-        //     numString1 += word[i];
-        // }
-        // for(int i = 2; i < 4; i++){
-        //     numString2 += word[i]; 
-        // }
-
-        // n1 = stoi(numString1); 
-        // n2 = stoi(numString2); 
-
-        // reverse(numString1.begin(), numString1.end()); 
-        // if(stoi(numString1) < n1){n1 = stoi(numString1);}
-        // reverse(numString2.begin(), numString2.end()); 
-        // if(stoi(numString2) < n2){n2 = stoi(numString2); }
-
-
-        // Sum = n1 + n2; 
-        // return Sum; 
-
-        int n1, num1, num2; 
+           int n1, num1, num2; 
         vector<int> nums; 
 
         while(true){
@@ -45,10 +18,7 @@ public:
         num1 = nums[0] * 10 + nums[2]; 
         num2 = nums[1] * 10 + nums[3]; 
 
-        return num1 + num2; 
-
-
-
+        return num1 + num2;    
         
     }
 };
