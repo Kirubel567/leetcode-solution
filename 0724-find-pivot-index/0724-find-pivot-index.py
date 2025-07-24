eca3826e1ed30ed71980f104427883d7c92a6fa0
Kirubel567 <kirubel.wubet1996@gmail.com>
2025-07-24 20:34:30 +0300

    Time: 7 ms (58.48%), Space: 18.7 MB (38.63%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        accumulate = 0
+        prefix_sum = []
+        for num in nums: 
+            accumulate += num
+            prefix_sum.append(accumulate)
+        
+
+        if prefix_sum[len(prefix_sum) - 1] - prefix_sum[0] == 0: 
+            return 0
+        for i in range(1, len(prefix_sum)): 
+            if prefix_sum[i - 1] == prefix_sum[len(prefix_sum) - 1] - prefix_sum[i]: 
+                return i
+
+        return -1
