55fc969bbe1bba302ae66de6789d6561f58bc793
Kirubel567 <kirubel.wubet1996@gmail.com>
2025-08-13 21:57:01 +0300

    Time: 8 ms (92.63%), Space: 21.3 MB (39.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,12 +3,7 @@ class Solution:
         mapp = defaultdict(list)
 
         for s in strs: 
-            count = [0] * 26
-
-            for ch in s: 
-                count[ord(ch) - ord("a")] += 1 
-            mapp[tuple(count)].append(s)
+            Sorted = sorted(s)
+            mapp[tuple(Sorted)].append(s)
 
         return list(mapp.values())
-
-
