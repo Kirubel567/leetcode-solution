94b30fe38003276b1a9633ee5f7b978b6354c9bc
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-08-30 23:30:30 +0300

    Time: 57 ms (8.8%), Space: 19.3 MB (50%) - LeetHub

---
+++ 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root: 
+            return []
+
+        stack = [root]
+        res = []
+
+        while stack: 
+            temp = stack.pop()
+            res.append(temp.val)
+            stack.extend(temp.children[::-1])
+        
+        return res
