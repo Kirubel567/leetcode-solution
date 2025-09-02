b4ce59e451d75ce5d030eedadfd47159d0184a80
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-09-02 18:48:23 +0300

    Time: 0 ms (100%), Space: 17.7 MB (66.9%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         for i in range(len(matrix)): 
-            for j in range(i+ 1, len(matrix)): 
+            for j in range(i + 1, len(matrix)): #here we start from i + 1 to avoid duplication around the diagonal, we are swapping around the diagonal and we could get the values from one side to swap no need to swap two times
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
         for i in range(len(matrix)): 
