c407b4e71eba0f143baba8756d369d71dac1bb2a
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-09-16 20:45:48 +0300

    Time: 45 ms (64.76%), Space: 17.6 MB (72.1%) - LeetHub

--- 3311-ant-on-the-boundary/3311-ant-on-the-boundary.py
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.py
@@ -2,7 +2,11 @@ class Solution:
     def returnToBoundaryCount(self, nums: List[int]) -> int:
         Sum, count = 0, 0
         for i in range(len(nums)): 
-            Sum += nums[i]
+            if nums[i] < 0: 
+                Sum -= -(nums[i])
+    
+            elif nums[i] > 0: 
+                Sum += nums[i]
             
             if Sum == 0: 
                 count += 1
