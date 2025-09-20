92b6cd3c8b1ec018da012e9357a22f6affd55d36
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-09-20 21:50:10 +0300

    Time: 9 ms (19.51%), Space: 17.7 MB (64.41%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,11 +4,42 @@ class Solution:
         mapp = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D":500, "M": 1000}
 
         Sum = 0
-        for i in range(len(s) - 1): 
-            if mapp[s[i]] >= mapp[s[i+1]]: 
-                Sum += mapp[s[i]]
-            elif mapp[s[i]] < mapp[s[i + 1]]: 
-                Sum -= mapp[s[i]]
+        i = 0 
+        while i < len(s):
+            if s[i] == "I" and i != len(s) -1:
+                if s[i + 1] == "V":   
+                    Sum += 4
+                    i += 2
+                elif s[i + 1] == "X":
+                    Sum += 9
+                    i += 2
+                else: 
+                    Sum += mapp[s[i]]
+                    i += 1
 
-        Sum += mapp[s[len(s)-1]]
+            elif s[i] == "X" and i != len(s) - 1:
+                if s[i + 1] == "L":   
+                    Sum += 40
+                    i += 2
+                elif s[i + 1] == "C":
+                    Sum += 90
+                    i += 2
+                else: 
+                    Sum += mapp[s[i]]
+                    i += 1
+            
+            elif s[i] == "C" and i != len(s) - 1:
+                if s[i + 1] == "D":   
+                    Sum += 400
+                    i += 2
+                elif s[i + 1] == "M":
+                    Sum += 900
+                    i += 2
+                else: 
+                    Sum += mapp[s[i]]
+                    i += 1
+            else: 
+                Sum += mapp[s[i]]
+                i += 1
+        
         return Sum 
