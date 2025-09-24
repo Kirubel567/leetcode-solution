18bf5eb5503e838991517c313abe674b71e4a8e1
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-09-24 23:03:51 +0300

    Time: 1 ms (44.93%), Space: 17.9 MB (45.64%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,22 +1,16 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums) - 1): 
-            if nums[i] == nums[i + 1]: 
-                multi = nums[i] * 2
-                nums[i] = multi
-                nums[i + 1] = 0
-            else: 
-                continue
+        for i in range(len(nums)-1): 
+            if nums[i] == nums[i+1]: 
+                nums[i] = nums[i] *2
+                nums[i +1] = 0
         
-        seeker = 0
-        holder = 0
-
-        while seeker < len(nums): 
+        #now use two pointers holder and seeker to group zeros to the end 
+        holder, seeker = 0, 0
+        while seeker< len(nums): 
             if nums[seeker] != 0: 
                 nums[seeker], nums[holder] = nums[holder], nums[seeker]
-                seeker+=1
-                holder+=1
-            else: 
-                seeker+=1
+                holder += 1
+            seeker += 1
 
         return nums
