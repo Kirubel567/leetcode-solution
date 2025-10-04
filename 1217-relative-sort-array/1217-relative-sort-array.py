167276c77d6b8773eca9c65473462ee2c16cdd7c
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-10-05 00:19:43 +0300

    Time: 0 ms (100%), Space: 18.1 MB (21.17%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,15 +1,29 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        result = []
+        #define a hashmap to store the frequency of the elements in arr1
+        freq = {}
 
-        for value in arr2:
-            for j in range(len(arr1)):
-                if arr1[j] == value:
-                    result.append(arr1[j])
-                    arr1[j] = -1
-        arr1.sort()
-       
-        for value in arr1:
-            if value != -1:
-                result.append(value)
-        return result
+        for i in range(len(arr1)): 
+            freq[arr1[i]] = freq.get(arr1[i], 0) +1
+        
+        #iterate through the second array and map them in a new arr
+        res = []
+
+        for j in range(len(arr2)): 
+            
+            count = freq[arr2[j]]
+            for i in range(count): 
+                res.append(arr2[j])
+        
+        rest = []
+        for key, value in freq.items(): 
+            if key not in arr2: 
+                count = value
+                if count > 0: 
+                    rest.extend([key] * count)
+        
+        
+        res.extend(sorted(rest))
+
+        
+        return res 
