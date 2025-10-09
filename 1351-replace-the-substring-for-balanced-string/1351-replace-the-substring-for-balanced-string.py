aa3b19518b57a4a4c805279d526238e29f441a45
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-10-09 17:58:33 +0300

    Time: 119 ms (79.95%), Space: 18 MB (80.21%) - LeetHub

---
+++ 1351-replace-the-substring-for-balanced-string/1351-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        n = len(s)//4
+        left, ans = 0, float("inf")
+        count = defaultdict(int)
+        for letter in s: 
+            count[letter] += 1
+
+        if max(count.values()) == n: 
+            return 0
+
+        for right in range(len(s)): 
+            count[s[right]] -=1
+
+            while left <= right and max(count.values()) <= n: 
+                ans = min(ans, right - left +1)
+
+                count[s[left]] += 1
+                left += 1
+
+        
+
+
+        return ans 
