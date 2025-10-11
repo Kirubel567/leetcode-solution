647728c5ccb4cdd0056613004428960dc5203e69
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-10-11 23:16:11 +0300

    Time: 42 ms (12.33%), Space: 25.3 MB (35.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums) 
-
-        #prefix 
+        prefix = []
+        res = [0] * len(nums)
         accumulator = 1
-        for i in range(len(nums)): 
-            res[i] *= accumulator 
-            accumulator *= nums[i]
-            
-        #postfix
-        prod = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= prod 
-            prod *= nums[i] 
-            
+        mapp = Counter(nums)
 
-        return res
+        #check if there is more than one 0
+        if mapp[0] > 1: 
+            return res
+
+        #create the prefix 
+        for i in range(len(nums)): 
+            if(nums[i] == 0):
+                continue
+            accumulator *= nums[i]
+            prefix.append(accumulator)
+        
+
+        #if a zero is found just return the product on that index
+        for i in range(len(nums)): 
+            if nums[i] == 0: 
+                res[i] = prefix[len(prefix) - 1]
+                return res
+
+        for i in range(len(prefix)): 
+            res[i] = prefix[len(prefix) - 1]//nums[i]
+        return res 
