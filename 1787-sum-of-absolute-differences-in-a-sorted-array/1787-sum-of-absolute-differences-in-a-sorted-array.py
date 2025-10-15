969cde21667d0b2a5d0bbe4d92650f1bfde17ef2
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-10-15 23:20:12 +0300

    Time: 654 ms (15.68%), Space: 33.1 MB (57.84%) - LeetHub

--- 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
@@ -3,22 +3,15 @@ class Solution:
         #this question has relation with the product of array except self 
         #you can try to do this question the naive way by finding the sum and subtracting n*nums[i] from the sum, but this only works for the starting and ending elements, the array is sorted so, we need to subtract the post_sum - nums[i]* n for the greater elements and nums[i]* n - pref_sum then add the two, instead of decrementing from the whole sum do it in parts(almost the same thing but this works)
         pref_sum, post_sum = 0, 0
-        pref = [0]*len(nums)
-        post=[0]*len(nums)
-
-        for i in range(len(nums)): 
-            pref[i] = pref_sum
-            pref_sum += nums[i]
-           
-
-        for i in range(len(nums) -1, -1, -1):
-            post[i] = post_sum 
-            post_sum += nums[i]
-            
         res = [0]*len(nums)
         for i in range(len(nums)): 
-            res[i] += (i*nums[i]) - pref[i]
-            res[i] += post[i] - (len(nums)-i-1)*nums[i]
+            res[i] += (i*nums[i]) - pref_sum
+            pref_sum += nums[i]
+           
+        
+        for i in range(len(nums) -1, -1, -1):
+            res[i] += post_sum - (len(nums)-i-1)*nums[i]
+            post_sum += nums[i]
         
         return res 
 
