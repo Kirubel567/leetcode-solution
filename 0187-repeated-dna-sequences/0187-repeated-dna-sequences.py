cc363235c671ac9b5ca7e23ca46a37647cc50823
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-04 23:36:46 +0300

    Time: 22 ms (72.97%), Space: 29.4 MB (15.15%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,21 +1,17 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         mapp = defaultdict(int)
-        #use two maps one for storing the freq of the substr and one for the number of each characters in the substring 
-        mapp[s[:10]] = 1
-        left = 0
-        res = []
-        for r in range(10, len(s)): 
-            left += 1
-            added = s[left:r+1]
-            if added in mapp and mapp[added] == 1: 
-                res.append(added)
-            mapp[added] += 1
+        res, seen = set(), set()
 
-       
-        
-        
-        return res
+        for l in range(len(s)-9): 
+            added = s[l:l+10]
+            if added in seen: 
+                res.add(added)
+            seen.add(added)
+
+        return list(res)
+
+
 
             
 
