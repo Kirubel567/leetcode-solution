a0fa784cedfbc99000dc277c30fed1df1403d497
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-04 23:28:41 +0300

    Time: 31 ms (30.59%), Space: 28.9 MB (75.03%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -4,17 +4,15 @@ class Solution:
         #use two maps one for storing the freq of the substr and one for the number of each characters in the substring 
         mapp[s[:10]] = 1
         left = 0
+        res = []
         for r in range(10, len(s)): 
             left += 1
             added = s[left:r+1]
+            if added in mapp and mapp[added] == 1: 
+                res.append(added)
             mapp[added] += 1
 
-
-        print(mapp)
-        res = []
-        for key in mapp: 
-            if mapp[key] > 1:
-                res.append(key)
+       
         
         
         return res
