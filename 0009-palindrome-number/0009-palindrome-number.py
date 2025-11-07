2b6193672c34f2ea435cf6a096818df7aa1dae8c
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-07 23:15:24 +0300

    Time: 13 ms (23.14%), Space: 17.6 MB (88.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,15 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        y = str(x)
-        if y == y[::-1]:
-            return True 
-        else:
+        #get the last digit until x is zero
+        #build the reversed number while getting the last digit using the modulo 
+        if x < 0: 
             return False
         
+        reversed = 0
+        compare = x 
+        while compare > 0: 
+            lastDigit = compare % 10
+            reversed = reversed * 10 + lastDigit
+            compare //=10
+
+        return reversed == x
