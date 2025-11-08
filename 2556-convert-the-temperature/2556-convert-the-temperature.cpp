dee77d5116b6e9a82770908fe6112bec001e2832
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-08 18:47:24 +0300

    Time: 0 ms (100%), Space: 8.5 MB (8.8%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.cpp
+++ 2556-convert-the-temperature/2556-convert-the-temperature.cpp
@@ -4,6 +4,7 @@ public:
         double Kelvin = celsius + 273.15; 
         double Fahrenheit = celsius * 1.80 + 32.00; 
         vector<double> ans = {Kelvin, Fahrenheit};
+        // double ans[2] = {Kelvin, Fahrenheit};
         
         return ans; 
 
