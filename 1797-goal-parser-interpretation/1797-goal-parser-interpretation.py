0212c5348226b8734da042e78038f4efc63053be
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-11 10:57:12 +0300

    Time: 32 ms (87.38%), Space: 17.5 MB (90.38%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,13 +1,18 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        word = ""
-        for i in range(len(command)): 
-            if command[i] == "G": 
-                word += command[i]
-            elif command[i] == "(": 
-                if i < len(command) - 1 and command[i + 1] == ")": 
-                    word += "o"
-                else: 
-                    word += "al"
-            
-        return word
+        ans = ""
+        i = 0
+        while i < len(command)-1:  
+            if command[i] == 'G': 
+                ans += 'G'
+                i+=1
+            else:
+                if command[i+1] == ')': 
+                    ans += 'o'
+                    i+=2
+                else:
+                    ans += 'al'
+                    i+= 4
+        if command[len(command)-1] == "G": 
+            ans += 'G'
+        return ans 
