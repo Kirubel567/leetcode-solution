de010ef2ed56c86c477743454dda70cebdb423c7
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-11 11:41:18 +0300

    Time: 3 ms (31.56%), Space: 17.9 MB (46.06%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -2,15 +2,16 @@ class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)-1): 
             if nums[i] == nums[i+1]: 
-                nums[i] = nums[i] *2
-                nums[i +1] = 0
+                nums[i+1] = 0
+                nums[i] *= 2
+        print(nums)
         
-        #now use two pointers holder and seeker to group zeros to the end 
-        holder, seeker = 0, 0
-        while seeker< len(nums): 
+
+        seeker, holder = 0, 0
+        while seeker < len(nums): 
             if nums[seeker] != 0: 
-                nums[seeker], nums[holder] = nums[holder], nums[seeker]
+                nums[holder], nums[seeker] = nums[seeker], nums[holder]
                 holder += 1
             seeker += 1
-
+        
         return nums
