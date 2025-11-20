db75a69c4b4035954c450656f60719bd452c07c1
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-20 17:42:22 +0300

    Time: 7 ms (97.73%), Space: 18.1 MB (30.86%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.py
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.py
@@ -1,13 +1,14 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        mapp = defaultdict(int)
-
-        for i in range(len(words)): 
-            key = ''.join(sorted(set(words[i])))
-            mapp[key]+=1
-        
-        count = 0
+        hashableArr = [frozenset(word) for word in words]
+        mapp = Counter(hashableArr)
+        ans = 0
         for key in mapp: 
-            if mapp[key] >1:
-                count += mapp[key] * (mapp[key] - 1) // 2
-        return count 
+            if mapp[key] > 1: 
+                ans += (mapp[key] * (mapp[key]-1))//2
+        
+        return ans 
+
+        
+
+
