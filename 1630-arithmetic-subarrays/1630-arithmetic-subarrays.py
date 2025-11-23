4282ee976fe4afa46f7dfa99235abfad471a17c6
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-23 20:05:22 +0300

    Time: 55 ms (37.19%), Space: 18 MB (36.96%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -1,27 +1,9 @@
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        #sorting is the way to find if they could be rearranged into arthimetic sequence 
-        #but after sorting you have to iterate through left[i] to right[i] 
-        #any better way????
-        tracker = 0
         res = []
-        while tracker < len(l): 
-            #get the subarray
-            store = sorted(nums[l[tracker]:r[tracker]+1])
-
-            #check if the subarray is in arthimetic sequence 
-            compare = store[1] - store[0]
-            checker = False
-            for j in range(1, len(store)): 
-                diff = store[j] - store[j-1]
-                if diff != compare: 
-                    res.append(False) 
-                    break
-            else: 
-                res.append(True)
-                
-            tracker +=1
+        for left, right in zip(l, r): 
+            store = sorted(nums[left:right+1])
+            diff = store[1]-store[0]
+            res.append(all(store[i]-store[i-1] == diff for i in range(1, len(store))))
+        
         return res 
-
-
-
