d890fe4217f774b76cf1b43ef6eb70bf9c79fbaa
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-28 21:54:00 +0300

    Time: 3 ms (98.93%), Space: 17.9 MB (92.31%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,11 +1,25 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        res = []
-        for i in range(len(boxes)):
-            store = 0 
-            for j in range(len(boxes)): 
-                if boxes[j] != '0': 
-                    store += abs(j-i)
-            res.append(store)
+        #when iterating through the array we are moving balls only one place as a time, but at a time we could move more than one balls so moves = 1, and numBall, then the total number of movement would be moves * numBall, but moves is 1 at a time so it would be numBall of movement at a time
+
+        #left portion each element 
+        moves, balls = 0, 0
+        res = [0]*len(boxes)
+        for i in range(len(boxes)): 
+            moves = moves + balls
+            res[i] = moves
+            
+            if boxes[i] == "1": 
+                balls +=1 
+
+        #right portion of each element 
+        moves, balls = 0, 0
+        for i in range(len(boxes)-1, -1, -1):
+            moves = moves + balls
+            res[i] += moves
+            
+            if boxes[i] == "1": 
+                balls +=1 
         
-        return res
+        return res 
+
