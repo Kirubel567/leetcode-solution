3e8444ef85c5883de25684857b9be91b6c5cc8f3
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-11-28 23:52:02 +0300

    Time: 0 ms (100%), Space: 17.6 MB (97.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,24 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        rcounter, wcounter, bcounter = 0, 0, 0
+        red, white, blue = 0, 0, 0
         for i in range(len(nums)): 
             if nums[i] == 0: 
-                rcounter += 1
-            elif nums[i] == 1: 
-                wcounter += 1
+                red += 1
+            elif nums[i] == 1:
+                white += 1
             else: 
-                bcounter += 1
-
-        for i in range(len(nums)): 
-            if rcounter >0: 
-                nums[i] = 0
-                rcounter-=1
-            elif wcounter >0: 
-                nums[i] = 1
-                wcounter -=1
-            else: 
-                nums[i] = 2
-                
-            
+                blue += 1
         
+        white += red 
+        blue += white
+        
+        for i in range(len(nums)): 
+            if i < red: 
+                nums[i] = 0
+            elif i < white: 
+                nums[i] =1
+            else: 
+                nums[i]=2
+        
+            
+
