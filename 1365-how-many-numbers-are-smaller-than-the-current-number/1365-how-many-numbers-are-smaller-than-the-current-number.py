ea024d1c83503ebc4b2af113dbe7a22009f700d3
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-12-08 23:17:05 +0300

    Time: 0 ms (100%), Space: 17.9 MB (43.64%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        idxHolder = defaultdict(int)
-        sor = sorted(nums)
-        for i in range(len(sor)): 
-            if sor[i] not in idxHolder:
-                idxHolder[sor[i]] = i
-
-
+        #[1,2,2,3,8]
+        mapp = defaultdict(int)
+        sorted_nums = sorted(nums)
+        for i in range(len(sorted_nums)): 
+            if sorted_nums[i] not in mapp: 
+                mapp[sorted_nums[i]] = i
+        
         res = []
         for i in range(len(nums)): 
-            res.append(idxHolder[nums[i]])
+           res.append(mapp[nums[i]])
+        return res 
 
-        return res
