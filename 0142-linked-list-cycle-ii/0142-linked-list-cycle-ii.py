7c78446ad024fa4cf1cdfafa0d5a845880db8b51
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2025-12-29 23:58:24 +0300

    Update 0142-linked-list-cycle-ii.py

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -2,7 +2,7 @@
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
-#         self.next = None
+#         self.next = None  
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
