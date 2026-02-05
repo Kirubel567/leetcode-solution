91ac60928a0ea5076cbc127985c88564ad7ec37b
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-02-05 15:11:44 +0300

    Time: 11 ms (24.39%), Space: 21.5 MB (5.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        #first sort the array
-        #iterate through the array and check if the value ain't equal to i
-        nums.sort()
-        for i in range(len(nums)): 
-            if i != nums[i]: 
-                return i
-
-        return nums[-1]+1
+        #use differnce for this 
+        check = {i for i in range(len(nums)+1)}
+        return (check - set(nums)).pop()
