9a453225d3ccd073c15119e8924a51befbf11be3
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-02-06 10:22:42 +0300

    Time: 0 ms (100%), Space: 19.3 MB (35.69%) - LeetHub

---
+++ 0722-remove-comments/0722-remove-comments.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        in_block = False
+        res =[]
+        buffer = ""
+        for line in source: 
+            i = 0
+            while i < len(line): 
+                if in_block:
+                    if i+1 < len(line) and line[i] == "*" and line[i+1] == '/': 
+                        in_block = False
+                        i+=2
+                    else: 
+                        i+=1
+                else:
+                    if i +1 < len(line) and line[i] == '/' and line[i+1] == "*": 
+                        in_block = True
+                        i+=2
+                    elif i+1 < len(line) and line[i] == '/' and line[i+1] == "/": 
+                        break
+                    else:
+                        buffer += line[i]
+                        i+=1
+            if not in_block and buffer: 
+                res.append(buffer)
+                buffer = ""
+
+        return res
