3dc568244ce1593dc3c2c68b678993cdec0471f5
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-02-09 16:04:10 +0300

    Time: 7 ms (54.14%), Space: 23 MB (30.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         mapp = Counter(nums)
-        arr1 = list(mapp.values())
-        arr1.sort()
-        arr = [0] * len(arr1)
-
-        for key in mapp: 
-            idx = arr1.index(mapp[key])
-            arr1[idx] = -1
-            arr[idx] = key
-
-        return arr[::-1][:k]
-
-       
+        ans = []
+        for key in mapp.most_common(k): 
+            ans.append(key[0])
+        return ans
