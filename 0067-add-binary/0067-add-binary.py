6f919f75918e7e37c7cb3eca2f065aebd5aad9fa
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-02-16 00:14:49 +0300

    Time: 0 ms (100%), Space: 19.6 MB (12.7%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,7 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        A, B = int(a, 2), int(b, 2) 
-        return bin(A + B)[2:]
+        ans = int(a, 2) + int(b, 2)
+        print(ans)
+        return bin(ans)[2:]
 
-
-        
